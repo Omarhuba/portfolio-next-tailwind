02c650e6839a600ecf396eed71321bdd7f2601f4
Omarhuba <omaruba@gmail.com>
2022-12-28 10:36:04 +0100

    created with next and tailwind

---
+++ public/projectsData.js
@@ -0,0 +1,102 @@
+import poster from '../assets/website-screenshots/poster.png'
+import sinus from '../assets/website-screenshots/sinus.png'
+import ewallet from '../assets/website-screenshots/e-wallet.png'
+import starwars from '../assets/website-screenshots/star-wars.png'
+import yummy from '../assets/website-screenshots/yummy.png'
+import sinusdesign from '../assets/website-screenshots/sinus-design.png'
+import design from '../assets/website-screenshots/design-principer.png'
+import sweuyghur from '../assets/website-screenshots/swe-uyghur.png'
+import food from '../assets/website-screenshots/food-order-app.png'
+
+
+const projectsData = [
+  {
+    id: 1,
+    name: "Swe-Uyghur .FC (Fullstack)",
+    img: sweuyghur,
+    desc: "Swe-Uyghur Fotball Club är min fotball club och Jag bygger den för att vi ska skapa konto, lägga inlägg, sälja våran producter osv . Projekt byggdes av Node.js, Express.js, Mongoose, MVC, Filuppladdning, JWT, Bcrypt, RBAC, Fullt fungerande endpoints. ",
+    codelink: "https://github.com/Omarhuba/Fullstack-Swe-Uyghur",
+    sitelink: "",
+  },
+  {
+    id: 2,
+    name: "Inga Bra Bygg AB (Backend)",
+    img: poster,
+    desc: "Inge Berglund driver en byggfirma som renoverar fasader för privatpersoner och företag. Projekt byggdes av Node.js, Express.js, Mongoose, MVC, Filuppladdning, JWT, Bcrypt, RBAC, Websockets, Fullt fungerande endpoints. ",
+    codelink: "https://github.com/Omarhuba/Backend-Slutproject-Grupp",
+    sitelink: "",
+  },
+  {
+    id: 3,
+    name: "Cold'n Yummy",
+    imgURL: "conldn yummy",
+    img: yummy,
+    desc: "Cold n Yummy™ vill ha en kampanjsida för framröstning av en ny glassmak. Användare väljer en smak och skickar med sitt namn och e-mail-adress för att rösta på den. Man ska även kunna se topplistan på de mest populära smakerna.",
+    codelink: "https://github.com/Omarhuba/coldn-yumyy-express-sequelize-",
+    sitelink: "",
+
+  },
+  {
+    id: 4,
+    name: "Food-Order-React-APP",
+    imgURL: "food-order",
+    img: food,
+    desc: "Food-Order-App är en enkel beställnings app. Den byggdes av React, Man kan beställa food, add item, remove item osv ",
+    codelink: "https://github.com/Omarhuba/Food-Order-App-react-",
+    sitelink: "",
+
+  },
+  {
+    id: 5,
+    name: "Sinus Web Shop(Vue)",
+    imgURL: "sinus",
+    img: sinus,
+
+    desc: "En e-handelsbutik byggd i Vue JS med VueX och routing. Kunder kan skapa ett konto för att spara sin information och det finns även ett admingränssnitt för att skapa, läsa, uppdatera och ta bort produkter och beställningar.",
+    codelink: "https://github.com/theodorwill/Slutprojekt-Sinus-Webshop",
+    sitelink: "",
+
+  },
+  {
+    id: 6,
+    name: "Figma-Design",
+    imgURL: "sinus-design",
+    img: sinusdesign,
+    desc: "Figma med Sinus webbshop Design, Använd Figma enligt Design principer",
+    codelink: "https://i.ibb.co/r5k0HzX/sinus-design.png",
+    sitelink: "",
+
+
+  },
+  {
+    id: 7,
+    name: "E-Wallet",
+    imgURL: "ewallet",
+    img: ewallet,
+    desc: "En digital plånbok app som samlar alla kreditkort. Det ska gå och se sina kreditkort samt lägga till nya.",
+    codelink: "https://github.com/Omarhuba/E-WALLET-VUE",
+    sitelink: "",
+
+  },
+  {
+    id: 8,
+    name: "Figma",
+    imgURL: "sinus-design",
+    img: design,
+    desc: "Figma med Olica Design Principer, Använd Style Guide, Atomer, Moleykyler, Organismer, och olika Template båda Desktop och Mobil ",
+    codelink: "https://i.ibb.co/TMWYFSB/design-principer.png",
+    sitelink: "",
+
+  },
+  {
+    id: 9,
+    name: "Star Wars",
+    imgURL: "star-wars",
+    img: starwars,
+    desc: "Vanilla Javascript med Star Wars API, Appen är en webbplats för en katalog över Star Wars karaktärer.",
+    codelink: "https://github.com/Omarhuba/Star-Wars-Native-Javascript",
+    sitelink: "https://youtu.be/rikf5ZNmD6I",
+
+  },
+];
+  export default projectsData;
